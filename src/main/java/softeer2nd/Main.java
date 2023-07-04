97d4bc052726e7c0de43ffa384e4826d46b69a43
jeongyeonKimm <wjddus9132@gmail.com>
2023-07-04 18:00:36 +0900

    feat: main 작성

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,7 +1,33 @@
 package softeer2nd;
 
+import softeer2nd.chess.Board;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Board board = new Board();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String command = sc.next();
+            if (command.equals("start")) {
+                startGame(board);
+            } else if (command.equals("end")) {
+                endGame();
+                break;
+            }
+        }
+    }
+
+    public static void startGame(Board board) {
+        System.out.println("Start Game!");
+        board.initialize();
+        System.out.println(board.print());
+    }
+
+    public static void endGame() {
+        System.out.println("End Game!");
     }
 }
