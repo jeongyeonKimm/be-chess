3bcf92d7923fb591cc8eda18ef8691c1cf68ff5c
jeongyeonKimm <wjddus9132@gmail.com>
2023-07-04 11:30:28 +0900

    refactor: BoardTest 코드 중복 제거

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,5 +1,7 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
 
@@ -7,27 +9,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
 
+    Board board;
+
+    @BeforeEach
+    public void create_board() {
+        board = new Board();
+    }
+
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        verifyInputPawn(white);
 
         Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        verifyInputPawn(black);
+    }
+
+    public void verifyInputPawn(Pawn pawn) {
+        int size = board.size();
+
+        board.add(pawn);
+        verifySize(size + 1);
+        verifyPawn(pawn);
+    }
+
+    public void verifySize(int size) {
+        assertEquals(size, board.size());
+    }
+
+    public void verifyPawn(Pawn pawn) {
+        assertEquals(pawn, board.findPawn(board.size() - 1));
     }
 
 //    @Test
+//    @DisplayName("Pawn 이외의 객체가 체스판에 입력되면 에러가 발생해야 한다.")
 //    public void create_Pawn_이외_객체() {
 //        Board board = new Board();
 //
 //        Integer num = new Integer("7");
 //        board.add(num);
-//        // 에러 발생
 //    }
 }
