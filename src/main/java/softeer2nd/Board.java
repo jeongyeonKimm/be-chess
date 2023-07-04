972bcb020b858758d8cc0f7f1163adb13cf4224e
jeongyeonKimm <wjddus9132@gmail.com>
2023-07-04 10:22:04 +0900

    feat: BoardTest 구현

--- src/main/java/softeer2nd/Board.java
+++ src/main/java/softeer2nd/Board.java
@@ -4,13 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private List<Pawn> pawns = new ArrayList<>();
+    private ArrayList<Pawn> pawns;
 
-    public Board(List<Pawn> pawns) {
+    public Board() {
+        pawns = new ArrayList<>();
+    }
+
+    public Board(ArrayList<Pawn> pawns) {
         this.pawns = pawns;
     }
 
     public List<Pawn> getPawns() {
         return pawns;
+    }
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int idx) {
+        if (pawns.size() == 0) {
+            return null;
+        }
+        return pawns.get(idx);
     }
 }
--- src/main/java/softeer2nd/Pawn.java
+++ src/main/java/softeer2nd/Pawn.java
@@ -2,8 +2,8 @@ package softeer2nd;
 
 public class Pawn {
 
-    private final String WHITE_COLOR = "white";
-    private final String BLACK_COLOR = "black";
+    static final String WHITE_COLOR = "white";
+    static final String BLACK_COLOR = "black";
     private String color;
 
     public Pawn() {
